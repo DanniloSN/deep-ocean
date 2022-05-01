892afcf37c1eb80f83c0b00f662802877c91e91b
Danilo de Souza Nunes <dannilosn@hotmai.com>
2022-05-01 11:32:26 -0300

    Added some real data

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-const foos = {
-    surface: [],
-    epipelagic: [
-        {
-            depth: 100,
-            title: 'Lorem ipsum dolor sit amet',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            depth: 200,
-            title: 'Lorem ipsum dolor sit amet',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            depth: 300,
-            title: 'Lorem ipsum dolor sit amet',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-    ]
-}
+const foos = [
+    {
+        depth: 253,
+        title: 'Free Dive Record',
+        description: 'On June 6, 2012, Herbert Nitsch, a free diver holding over 30 world records, set the bar for the deepest free dive ever by descending 253 meters in Greece.'
+    },
+    {
+        depth: 2992,
+        title: 'Whale Dive Record',
+        description: "Whales are accomplished divers. The deepest whale dive recorded so far was made by a Cuvier's beaked whale. A 2014 study used satellite-linked tags to follow the dives of eight beaked whales off the southern California coast. The deepest recorded dive was 2,992 metres, breaking the record for diving mammals."
+    },
+    {
+        depth: 3962.4,
+        title: 'RMS Titanic',
+        description: 'RMS Titanic was a British passenger liner, operated by the White Star Line, which sank in the North Atlantic Ocean on 15 April 1912 after striking an iceberg during her maiden voyage from Southampton, UK, to New York City. Of the estimated 2,224 passengers and crew aboard, more than 1,500 died, which made the sinking possibly one of the deadliest for a single ship up to that time.'
+    }
+]
 
 const generateFoo = (fooData) => {
     const foo = document.createElement('div')
@@ -28,7 +25,7 @@ const generateFoo = (fooData) => {
     fooCard.className = 'foo-card'
 
     const fooCardTitle = document.createElement('h1')
-    fooCardTitle.innerHTML = `${fooData.depth}m`
+    fooCardTitle.innerHTML = `${fooData.title} - ${fooData.depth}m`
 
     const fooCardDescription = document.createElement('p')
     fooCardDescription.innerHTML = fooData.description
@@ -42,7 +39,7 @@ const generateFoo = (fooData) => {
     return foo
 }
 
-foos.epipelagic.forEach(foo => {
+foos.forEach(foo => {
     const oceanLevel = document.getElementById('oceanLevel')
     oceanLevel.appendChild(generateFoo(foo))
 });
